da4556a2ad1046631e22ef50b49e45795e05131d
madking55 <madking55@gmail.com>
2018-11-24 15:58:13 +0100

    fixed simple_form for creating new sock

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -1,2 +1,4 @@
 class Sock < ApplicationRecord
+    enum kind: [:'no show', :liner, :ped, :quarter, :'mid calf', :'over the calf']
+    enum color: [:blue, :green, :black, :white, :yellow, :red, :other]
 end
