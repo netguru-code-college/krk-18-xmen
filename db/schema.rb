951fa6674c4acb8275c99d57638bbb6cb2bf1590
Czerwonka <alicja.czerwonka@gmail.com>
2018-11-24 16:52:00 +0100

    Conflicts fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_24_111245) do
 
   # These are extensions that must be enabled in order to support this database
@@ -24,7 +23,6 @@ ActiveRecord::Schema.define(version: 2018_11_24_111245) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "requests", force: :cascade do |t|
     t.string "photo"
     t.text "description"
@@ -35,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_11_24_111245) do
     t.integer "requester_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
