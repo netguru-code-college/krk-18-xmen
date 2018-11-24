32843f62eaa76107cd68cac9a3c2bd95a27f6516
madking55 <madking55@gmail.com>
2018-11-24 12:54:00 +0100

    add socks model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_171158) do
+ActiveRecord::Schema.define(version: 2018_11_24_114450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2018_07_17_171158) do
     t.string "name"
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "socks", force: :cascade do |t|
+    t.text "description"
+    t.integer "size"
+    t.integer "kind"
+    t.integer "color"
+    t.string "material"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
