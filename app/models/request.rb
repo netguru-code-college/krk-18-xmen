da5d1528272d763baa487a53c4d4db830b3cf1a5
Czerwonka <alicja.czerwonka@gmail.com>
2018-11-24 15:11:17 +0100

    Add form for request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
-  enum kind: [:noshow, :liner, :ped, :quarter, :midcalf, :overthecalf]
-  enum color: [:blue, :green, :black, :white, :yellow, :red, :other]
+  enum kind_type: [:'no show', :liner, :ped, :quarter, :'mid calf', :'over the calf']
+  enum color_type: [:blue, :green, :black, :white, :yellow, :red, :other]
   mount_uploader :photo, PhotoUploader
 end
