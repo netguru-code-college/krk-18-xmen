9395ece30e688a2d4875ba11a2301db89c637662
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2018-11-25 15:03:02 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   resources :socks do
     get 'mine', to: :mine, on: :collection
-	end
+  end
   resources :requests  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
-	authenticated :user do
-		root 'dashboard#index'
-	end
+  authenticated :user do
+    root 'dashboard#index'
+  end
 
-	root 'dashboard#guest'
+  root 'dashboard#guest'
 end
