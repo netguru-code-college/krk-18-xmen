45979f5422a7d6851473536d2e7bb96ef44ff756
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2018-11-25 09:57:18 +0100

    add guest dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
   end
+
+  def guest
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-	root 'dashboard#index'
-  	devise_for :users
+	devise_for :users
+
+	get '/dashboard', to: 'dashboard#index'
+
+	authenticated :user do
+		root 'dashboard#index'
+	end
+
+	root 'dashboard#guest'
+	
 end
