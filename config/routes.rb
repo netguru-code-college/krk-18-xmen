50cdcc3c7d159175fffda24567ce0e3bee56f920
Czerwonka <alicja.czerwonka@gmail.com>
2018-11-25 13:53:58 +0100

    Impove views, add partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :socks
   resources :requests  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'mine' => 'requests#mine'
 
   devise_for :users
 
