443b4935e223f561f5861f49376b4dc302fb9467
madking55 <madking55@gmail.com>
2018-11-25 11:18:57 +0100

    fixed indentation and empty lines

--- app/controllers/socks_controller.rb
+++ app/controllers/socks_controller.rb
@@ -2,7 +2,7 @@ class SocksController < ApplicationController
 
     def index
         @socks = Sock.all
-      end
+    end
 
     def show
         @sock = Sock.find(params[:id])
@@ -14,40 +14,33 @@ class SocksController < ApplicationController
 
     def create
         @sock = Sock.new(socks_params)
-       
         @sock.save
         redirect_to @sock
-      end
+    end
 
     def edit
         @sock = Sock.find(params[:id])
-     
     end
-
 
     def update
-  @sock = Sock.find(params[:id])
- 
-  if @sock.update(socks_params)
-    redirect_to @sock
-  else
-    render 'edit'
-  end
-end
-
-
-def destroy
     @sock = Sock.find(params[:id])
-    @sock.destroy
-   flash[:success] = "Sock deleted"
-    redirect_to socks_path
-  end
-
-
-   
-    private
-    def socks_params
-      params.require(:sock).permit(:description, :size, :kind, :color, :material)
+  
+    if @sock.update(socks_params)
+      redirect_to @sock
+    else
+      render 'edit'
+      end
     end
 
+    def destroy
+      @sock = Sock.find(params[:id])
+      @sock.destroy
+    flash[:success] = "Sock deleted"
+      redirect_to socks_path
+    end
+
+    private
+      def socks_params
+        params.require(:sock).permit(:description, :size, :kind, :color, :material)
+      end
 end
