aa793cd13ef267d4f1bafa97844b892d02dc980c
Czerwonka <alicja.czerwonka@gmail.com>
2018-11-26 17:34:55 +0100

    Add photo to socks, change dashbord view

--- app/controllers/socks_controller.rb
+++ app/controllers/socks_controller.rb
@@ -1,51 +1,52 @@
 class SocksController < ApplicationController
 
-    def index
-      @socks = Sock.all.order(:created_at).page(params[:page])
-    
-    end
+  def index
+    @socks = Sock.all.order(:created_at).page(params[:page])
+  end
 
-    def show
-        @sock = Sock.find(params[:id])
-    end
-
-    def new
-        @sock = Sock.new
-    end
-
-    def create
-        @sock = current_user.socks.build(socks_params)
-        @sock.save
-        redirect_to @sock
-    end
-
-    def edit
-        @sock = Sock.find(params[:id])
-    end
-
-    def update
+  def show
     @sock = Sock.find(params[:id])
-  
+  end
+
+  def new
+    @sock = Sock.new
+  end
+
+  def create
+    @sock = current_user.socks.build(socks_params)
+    @sock.save
+    redirect_to @sock
+  end
+
+  def edit
+    @sock = Sock.find(params[:id])
+  end
+
+  def update
+    @sock = Sock.find(params[:id])
+
     if @sock.update(socks_params)
       redirect_to @sock
     else
       render 'edit'
-      end
     end
+  end
 
-    def mine
-      @socks = current_user.socks.page(params[:page])
-    end
+  def mine
+    @socks = current_user.socks.page(params[:page])
+  end
 
-    def destroy
-      @sock = Sock.find(params[:id])
-      @sock.destroy
+  def destroy
+    @sock = Sock.find(params[:id])
+    @sock.destroy
     flash[:success] = "Sock deleted"
-      redirect_to socks_path
-    end
+    redirect_to socks_path
+  end
 
-    private
-      def socks_params
-        params.require(:sock).permit(:description, :size, :kind, :color, :material, :user_id)
-      end
+  private
+
+  def socks_params
+    params.require(:sock).permit(:description, :size, :kind, :color, :material, :user_id, :photo)
+  end
+  
 end
